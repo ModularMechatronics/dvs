265bcaf3f9a0a7232144538c8ba7ab95cc526321
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-04-07 07:58:18 +0200

    Added main application

---
+++ cpp/main_application/main_window.cpp
@@ -0,0 +1,26 @@
+#include "main_window.h"
+
+#include <unistd.h>
+
+#include <csignal>
+#include <iostream>
+
+MainWindow::MainWindow(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxPoint(30, 30), wxSize(700, 700))
+{
+
+}
+
+void MainWindow::OnClose(wxCloseEvent& event)
+{
+    (void)event;
+    if (wxMessageBox("Are you sure you want to exit?",
+                     "Please confirm",
+                     wxICON_QUESTION | wxYES_NO) != wxYES)
+    {
+        return;
+    }
+
+    std::cout << "Window close" << std::endl;
+    Destroy();
+}
