dcdf43d8fa7114bce3366ee8238dc5094ca25e32
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-05-26 07:12:01 +0200

    Changes to custom button

--- cpp/main_application/custom_button.h
+++ cpp/main_application/custom_button.h
@@ -9,6 +9,18 @@
 #include <wx/menu.h>
 #include <wx/wx.h>
 
+
+constexpr int kCirclePeriod = 20;
+constexpr int kEnteredPeriod = 10;
+constexpr int kNumCircleIterationsMax = 20;
+constexpr int kNumEnteredIterationsMax = 10;
+constexpr int kInitialAlpha = 127;
+constexpr int kShadowMargin = 5;
+constexpr int kColorInc = 2;
+constexpr int kCircleAlphaDec = 10;
+constexpr double kCircleRadiusInc = 4.0;
+constexpr double kInitialCircleRadius = 10.0;
+
 class CustomButton : public wxPanel
 {
 public:
@@ -21,9 +33,8 @@ public:
 
         size_ = size;
         pos_ = pos;
-        shadow_margin_ = 5;
-        circle_radius_ = 10.0;
-        button_size_ = wxSize(size.GetWidth() - shadow_margin_, size.GetHeight() - shadow_margin_);
+        circle_radius_ = kInitialCircleRadius;
+        button_size_ = wxSize(size.GetWidth() - kShadowMargin, size.GetHeight() - kShadowMargin);
 
         original_color_ = wxColor(30, 174, 152);
         button_color_ = original_color_;
@@ -35,12 +46,12 @@ public:
         circle_pen = wxPen(wxColor(255, 255, 255, 0), 1);
         circle_brush = wxBrush(wxColor(255, 255, 255, 127), wxBRUSHSTYLE_SOLID);
         button_rect = wxRect(0, 0, button_size_.GetWidth(), button_size_.GetHeight());
-        shadow_rect = wxRect(shadow_margin_, shadow_margin_, button_size_.GetWidth(), button_size_.GetHeight());
+        shadow_rect = wxRect(kShadowMargin, kShadowMargin, button_size_.GetWidth(), button_size_.GetHeight());
         draw_circle_ = false;
-        circle_alpha_ = 127;
+        circle_alpha_ = kInitialAlpha;
 
         mouse_click_pos_ = wxPoint(30, 30);
-        color_inc_ = -2;
+        color_inc_ = -kColorInc;
 
         Bind(wxEVT_PAINT, &CustomButton::OnPaint, this);
         Bind(wxEVT_LEFT_DOWN, &CustomButton::mouseLeftPressed, this);
@@ -49,7 +60,7 @@ public:
         // Bind(wxEVT_SIZE, &CustomButton::OnSize, this);
         click_timer_.Bind(wxEVT_TIMER, &CustomButton::OnClickedTimer, this);
         entered_timer_.Bind(wxEVT_TIMER, &CustomButton::OnEnteredTimer, this);
-        
+        exited_timer_.Bind(wxEVT_TIMER, &CustomButton::OnExitedTimer, this);
     }
 
 private:
@@ -60,6 +71,7 @@ private:
     void mouseExited(wxMouseEvent& event);
     void OnClickedTimer(wxTimerEvent& event);
     void OnEnteredTimer(wxTimerEvent& event);
+    void OnExitedTimer(wxTimerEvent& event);
 
     void drawWithCircle();
     void drawNormal();
@@ -71,7 +83,6 @@ private:
     double circle_radius_;
     int num_timer_iterations_clicked_;
     int num_timer_iterations_entered_;
-    int shadow_margin_;
     bool draw_circle_;
     wxColour original_color_;
     wxColour button_color_;
@@ -89,6 +100,7 @@ private:
 
     wxTimer click_timer_;
     wxTimer entered_timer_;
+    wxTimer exited_timer_;
 };
 
 inline void CustomButton::drawNormal()
@@ -124,42 +136,60 @@ inline void CustomButton::OnPaint(wxPaintEvent& event)
 inline void CustomButton::mouseEntered(wxMouseEvent& event)
 {
     wxSetCursor(wxCursor(wxCURSOR_HAND));
-    if(!entered_timer_.IsRunning())
+
+    exited_timer_.Stop();
+    while(exited_timer_.IsRunning())
     {
-        color_inc_ = -2;
-        entered_timer_.Start(10);
+        wxMilliSleep(1);
     }
+    // if(!entered_timer_.IsRunning())
+    // {
+        color_inc_ = -kColorInc;
+        entered_timer_.Start(kEnteredPeriod);
+    // }
 }
 
 inline void CustomButton::mouseExited(wxMouseEvent& event)
 {
     wxSetCursor(wxCursor(wxCURSOR_ARROW));
-    click_timer_.Stop();
+
     entered_timer_.Stop();
-    draw_circle_ = false;
-    num_timer_iterations_entered_ = 0;
-    color_inc_ = 2;
-    entered_timer_.Start(10);
+    while(entered_timer_.IsRunning())
+    {
+        wxMilliSleep(1);
+    }
+
+    // draw_circle_ = false;
+    std::cout << num_timer_iterations_entered_ << std::endl;
+
+    // num_timer_iterations_entered_ = 0;
+    color_inc_ = kColorInc;
+    exited_timer_.Start(kEnteredPeriod);
     Refresh();
 }
 
 inline void CustomButton::mouseLeftPressed(wxMouseEvent& event)
 {
-    if(!click_timer_.IsRunning())
+    if(click_timer_.IsRunning())
     {
-        circle_radius_ = 10.0;
-        mouse_click_pos_ = event.GetPosition();
-        click_timer_.Start(20);
+        click_timer_.Stop();
+        num_timer_iterations_clicked_ = 0;
+        circle_alpha_ = kInitialAlpha;
+        draw_circle_ = false;
     }
+
+    circle_radius_ = kInitialCircleRadius;
+    mouse_click_pos_ = event.GetPosition();
+    click_timer_.Start(kCirclePeriod);
 }
 
 inline void CustomButton::OnClickedTimer(wxTimerEvent& event)
 {
-    if(num_timer_iterations_clicked_ > 20)
+    if(num_timer_iterations_clicked_ > kNumCircleIterationsMax)
     {
         click_timer_.Stop();
         num_timer_iterations_clicked_ = 0;
-        circle_alpha_ = 127;
+        circle_alpha_ = kInitialAlpha;
         draw_circle_ = false;
     }
     else
@@ -167,23 +197,14 @@ inline void CustomButton::OnClickedTimer(wxTimerEvent& event)
         draw_circle_ = true;
         num_timer_iterations_clicked_++;
     }
+
     Refresh();
-    circle_radius_ += 4.0;
-    circle_alpha_ = std::max(0, circle_alpha_ - 10);
-    
+    circle_radius_ += kCircleRadiusInc;
+    circle_alpha_ = std::max(0, circle_alpha_ - kCircleAlphaDec);
 }
 
-inline void CustomButton::OnEnteredTimer(wxTimerEvent& event)
+inline void CustomButton::OnExitedTimer(wxTimerEvent& event)
 {
-    if(num_timer_iterations_entered_ > 10)
-    {
-        entered_timer_.Stop();
-        num_timer_iterations_entered_ = 0;
-    }
-    else
-    {
-        num_timer_iterations_entered_++;
-    }
     button_color_ = wxColour(button_color_.Red() + color_inc_,
                              button_color_.Green() + color_inc_,
                              button_color_.Blue() + color_inc_);
@@ -191,6 +212,38 @@ inline void CustomButton::OnEnteredTimer(wxTimerEvent& event)
     brush = wxBrush(button_color_, wxBRUSHSTYLE_SOLID);
 
     Refresh();
+
+    if(num_timer_iterations_entered_ <= 0)
+    {
+        exited_timer_.Stop();
+        num_timer_iterations_entered_ = 0;
+        button_color_ = original_color_;
+    }
+    else
+    {
+        num_timer_iterations_entered_--;
+    }
+}
+
+inline void CustomButton::OnEnteredTimer(wxTimerEvent& event)
+{
+    button_color_ = wxColour(button_color_.Red() + color_inc_,
+                             button_color_.Green() + color_inc_,
+                             button_color_.Blue() + color_inc_);
+    pen = wxPen(button_color_, 1);
+    brush = wxBrush(button_color_, wxBRUSHSTYLE_SOLID);
+
+    Refresh();
+
+    if(num_timer_iterations_entered_ >= (kNumEnteredIterationsMax - 1))
+    {
+        entered_timer_.Stop();
+        // num_timer_iterations_entered_ = 0;
+    }
+    else
+    {
+        num_timer_iterations_entered_++;
+    }
 }
 
 inline void CustomButton::OnSize(wxSizeEvent& event)
