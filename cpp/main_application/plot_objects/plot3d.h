e37686a9b51298fe6ee7da4ee708a978b2cb8bfa
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-06-10 07:17:48 +0200

    Added arbitrary datatype for plot2 and plot3

--- cpp/main_application/plot_objects/plot3d.h
+++ cpp/main_application/plot_objects/plot3d.h
@@ -36,31 +36,8 @@ Plot3D::Plot3D(std::unique_ptr<const ReceivedData> received_data, const Function
         throw std::runtime_error("Invalid function type for Plot3D!");
     }
 
-    points_ptr_ = new uint8_t[num_elements_ * num_bytes_per_element_ * 3];
+    points_ptr_ = convertData3DOuter(data_ptr_, data_type_, num_elements_, num_bytes_per_element_, num_bytes_for_one_vec_);
 
-    size_t idx_x = 0;
-    size_t idx_y = num_bytes_per_element_;
-    size_t idx_z = 2 * num_bytes_per_element_;
-
-    for(size_t k = 0; k < num_elements_; k++)
-    {
-        const size_t idx_0 = k * num_bytes_per_element_;
-        const size_t idx_1 = num_bytes_for_one_vec_ + k * num_bytes_per_element_;;
-        const size_t idx_2 = num_bytes_for_one_vec_ * 2 + k * num_bytes_per_element_;
-        const uint8_t* const tmp_ptr_0 = &(data_ptr_[idx_0]);
-        const uint8_t* const tmp_ptr_1 = &(data_ptr_[idx_1]);
-        const uint8_t* const tmp_ptr_2 = &(data_ptr_[idx_2]);
-
-        for(size_t i = 0; i < num_bytes_per_element_; i++)
-        {
-            points_ptr_[idx_x + i] = tmp_ptr_0[i];
-            points_ptr_[idx_y + i] = tmp_ptr_1[i];
-            points_ptr_[idx_z + i] = tmp_ptr_2[i];
-        }
-        idx_x += num_bytes_per_element_ * 3;
-        idx_y += num_bytes_per_element_ * 3;
-        idx_z += num_bytes_per_element_ * 3;
-    }
 }
 
 void Plot3D::findMinMax()
@@ -75,13 +52,13 @@ void Plot3D::visualize()
         visualize_has_run_ = true;
         glGenBuffers(1, &buffer_idx_);
         glBindBuffer(GL_ARRAY_BUFFER, buffer_idx_);
-        glBufferData(GL_ARRAY_BUFFER, num_bytes_per_element_ * num_elements_ * 3, points_ptr_, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * num_elements_ * 3, points_ptr_, GL_STATIC_DRAW);
     }
     setColor(color_);
     setLinewidth(line_width_);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, buffer_idx_);
-    glVertexAttribPointer(0, 3, dataTypeToGLInt(data_type_), GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
     glDrawArrays(GL_LINE_STRIP, 0, num_elements_);
     glDisableVertexAttribArray(0);
