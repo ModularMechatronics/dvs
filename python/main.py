6899b4b5aaf57d4db86e6da747cb7f307161c2ca
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-06-13 16:37:10 +0200

    Added first draft of python interface

---
+++ python/main.py
@@ -0,0 +1,10 @@
+
+import dvs
+import numpy as np
+
+if __name__ == "__main__":
+    x = np.linspace(0, 3, 100, dtype=np.float32)
+    y = np.sin(x * 5.0)
+
+    # dvs.plot(x, y, color=dvs.Color(23, 25, 66), alpha=100, line_width=13)
+    dvs.plot(x, y)
