a7d1bb5c2b8d9207733bb9f27cffa2b74742f480
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-07-19 09:04:55 +0200

    Started C interface

---
+++ interfaces/c/communication.h
@@ -0,0 +1,87 @@
+#ifndef COMMUNICATION_H_
+#define COMMUNICATION_H_
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <net/if.h>
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <chrono>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#define DVS_PORT_NUM 9547
+
+inline int& Var_socket_file_descr()
+{
+    static int socket_file_descr;
+    return socket_file_descr;
+}
+
+class UdpClient
+{
+private:
+    int sock_file_descr;
+    struct sockaddr_in tx_addr;
+    struct sockaddr* tx_addr_ptr;
+    socklen_t client_len;
+
+public:
+    UdpClient() : sock_file_descr(-1), tx_addr_ptr(nullptr) {}
+
+    UdpClient(const uint64_t port_num)
+    {
+        if ((sock_file_descr = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        {
+            perror("socket failed");
+            sock_file_descr = -1;
+        }
+
+        memset(&tx_addr, 0, sizeof(tx_addr));
+        tx_addr.sin_family = AF_INET;
+        tx_addr.sin_port = htons(port_num);
+        tx_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        tx_addr_ptr = (struct sockaddr*)&tx_addr;
+    }
+
+    ~UdpClient()
+    {
+        close(sock_file_descr);
+    }
+
+    int receiveData(char data[256])
+    {
+        client_len = sizeof(tx_addr);
+        const int num_received_bytes = recvfrom(sock_file_descr, data, 256, 0, (struct sockaddr*)&tx_addr, &client_len);
+        return num_received_bytes;
+    }
+
+    void sendData(const uint8_t* const data, const uint64_t num_bytes)
+    {
+        if (sock_file_descr == -1)
+        {
+            perror("Invalid socket!");
+        }
+        else if (sendto(sock_file_descr, data, num_bytes, 0, tx_addr_ptr, sizeof(tx_addr)) < 0)
+        {
+            perror("sendto failed");
+        }
+    }
+};
+
+#endif
---
+++ interfaces/c/enumerations.h
@@ -0,0 +1,136 @@
+#ifndef DVS_ENUMS_H_
+#define DVS_ENUMS_H_
+
+#include <stdint.h>
+
+typedef enum S_FunctionHeaderObjectType
+{
+    FHOT_FUNCTION,
+    FHOT_NUM_BUFFERS_REQUIRED,
+    FHOT_NUM_BYTES,
+    FHOT_DATA_STRUCTURE,
+    FHOT_BYTES_PER_ELEMENT,
+    FHOT_DATA_TYPE,
+    FHOT_NUM_CHANNELS,
+    FHOT_NUM_ELEMENTS,
+    FHOT_NUM_VERTICES,
+    FHOT_NUM_INDICES,
+    FHOT_DIMENSION_2D,
+    FHOT_HAS_PAYLOAD,
+    FHOT_AZIMUTH,
+    FHOT_ELEVATION,
+    FHOT_AXIS_MIN_MAX_VEC,
+    FHOT_POS2D,
+    FHOT_FIGURE_NUM,
+    FHOT_PARENT_NAME,
+    FHOT_PARENT_TYPE,
+    FHOT_ELEMENT_NAME,
+    FHOT_GUI_ELEMENT_TYPE,
+    FHOT_PROPERTY,
+    FHOT_UNKNOWN
+} FunctionHeaderObjectType;
+
+typedef enum S_PropertyType
+{
+    PT_LINE_WIDTH,
+    PT_ALPHA,
+    PT_NAME,
+    PT_LINE_STYLE,
+    PT_COLOR,
+    PT_EDGE_COLOR,
+    PT_FACE_COLOR,
+    PT_COLOR_MAP,
+    PT_POINT_SIZE,
+    PT_PERSISTENT,
+    PT_UNKNOWN
+} PropertyType;
+
+typedef enum S_Function
+{
+    F_DRAW_LINE3D,
+    F_PLANE_XY,
+    F_PLANE_XZ,
+    F_PLANE_YZ,
+    F_GRID_ON,
+    F_GRID_OFF,
+    F_PLOT2,
+    F_PLOT3,
+    F_SET_CURRENT_ELEMENT,
+    F_CREATE_NEW_ELEMENT,
+    F_NEW_ELEMENT,
+    F_SCATTER2,
+    F_SCATTER3,
+    F_DRAW_LINE_BETWEEN_POINTS_3D,
+    F_POLYGON_FROM_4_POINTS,
+    F_DRAW_TRIANGLES_3D,
+    F_DRAW_TRIANGLE_3D,
+    F_DRAW_MESH,
+    F_HOLD_ON,
+    F_HOLD_OFF,
+    F_POSITION,
+    F_SURF,
+    F_IM_SHOW,
+    F_AXES_2D,
+    F_AXES_3D,
+    F_VIEW,
+    F_CLEAR,
+    F_SOFT_CLEAR,
+    F_UNKNOWN
+} Function;
+
+typedef enum S_DataType
+{
+    DT_FLOAT,
+    DT_DOUBLE,
+    DT_INT8,
+    DT_INT16,
+    DT_INT32,
+    DT_INT64,
+    DT_UINT8,
+    DT_UINT16,
+    DT_UINT32,
+    DT_UINT64,
+    DT_UNKNOWN
+} DataType;
+
+typedef enum S_DataStructure
+{
+    DS_MATRIX,
+    DS_VECTOR,
+    DS_IMAGE,
+    DS_TEXT,
+    DS_UNKNOWN
+} DataStructure;
+
+typedef enum S_ColorMapType
+{
+    CMT_JET,
+    CMT_RAINBOW,
+    CMT_MAGMA,
+    CMT_VIRIDIS,
+    CMT_UNKNOWN
+} ColorMapType;
+
+typedef enum S_ElementType
+{
+    ET_PLOT,
+    ET_IMAGE,
+    ET_WORLD_3D,
+    ET_BUTTON,
+    ET_TEXT_FIELD,
+    ET_UNKNOWN
+} ElementType;
+
+typedef enum S_ElementParent
+{
+    EP_TAB,
+    EP_WINDOW,
+    EP_UNKNOWN
+} ElementParent;
+
+/*namespace properties
+{
+constexpr internal::PropertyType PERSISTENT = internal::PropertyType::PERSISTENT;
+}*/
+
+#endif
