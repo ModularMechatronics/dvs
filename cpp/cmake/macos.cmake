8bc210a3952175ff5da3c79bcf7f74057fc3b4cc
Daniel Pihlquist <danielpi@danielsmacbook.local>
2021-10-29 06:07:41 +0200

    Added cmake thingy

--- cpp/cmake/macos.cmake
+++ cpp/cmake/macos.cmake
@@ -6,14 +6,17 @@ set(wxWidgets_LIBRARIES -lwx_osx_cocoau_core-3.1
                         -lwx_baseu-3.1
                         -lwx_osx_cocoau_gl-3.1
                         -lwx_osx_cocoau_adv-3.1
+                        -lwxpng-3.1
                         )
-                        # -lwxpng-3.1
+
 set(PLATFORM_LIBRARIES pthread
                        iconv
                        png
                        z
                        "-framework System"
                        "-framework IOKit"
+                       "-framework OpenGL"
+                       "-framework AGL"
                        "-framework Carbon"
                        "-framework Cocoa"
                        "-framework QuartzCore")
