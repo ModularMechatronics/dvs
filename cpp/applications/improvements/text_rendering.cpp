1f6a71bd06da4db3cfe64c659aed6351b8b2e19a
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-12-17 08:02:56 +0100

    Grid numbers working for X axis

--- cpp/applications/improvements/text_rendering.cpp
+++ cpp/applications/improvements/text_rendering.cpp
@@ -48,11 +48,8 @@ void initText2D(const char * texturePath){
 
 }
 
-bool has_loaded = false;
-
 GLuint loadDDS(const char * imagepath)
 {
-    has_loaded = false;
 	unsigned char header[124];
 
 	FILE *fp; 
@@ -146,62 +143,60 @@ size_t num_vertices;
 void printText2D(const char* text, const float x, const float y, const float size)
 {
 	unsigned int length = strlen(text);
-    if(!has_loaded)
-    {
-        has_loaded = true;
     
-        // Fill buffers
-        std::vector<glm::vec2> vertices;
-        std::vector<glm::vec2> UVs;
-        for (int i=0 ; i<length ; i++ ){
-            
-            glm::vec2 vertex_up_left    = glm::vec2( x+i*size     , y+size );
-            glm::vec2 vertex_up_right   = glm::vec2( x+i*size+size, y+size );
-            glm::vec2 vertex_down_right = glm::vec2( x+i*size+size, y      );
-            glm::vec2 vertex_down_left  = glm::vec2( x+i*size     , y      );
+	// Fill buffers
+	std::vector<glm::vec2> vertices;
+	std::vector<glm::vec2> UVs;
+	for (int i=0 ; i < length; i++ )
+	{
 
-            vertices.push_back(vertex_up_left   );
-            vertices.push_back(vertex_down_left );
-            vertices.push_back(vertex_up_right  );
+		const float inc = static_cast<float>(i) * size * 0.75f;
+		glm::vec2 vertex_up_left    = glm::vec2(x + inc, y + size);
+		glm::vec2 vertex_up_right   = glm::vec2(x + inc + size, y + size );
+		glm::vec2 vertex_down_right = glm::vec2(x + inc + size, y);
+		glm::vec2 vertex_down_left  = glm::vec2(x + inc, y);
 
-            vertices.push_back(vertex_down_right);
-            vertices.push_back(vertex_up_right);
-            vertices.push_back(vertex_down_left);
+		vertices.push_back(vertex_up_left   );
+		vertices.push_back(vertex_down_left );
+		vertices.push_back(vertex_up_right  );
 
-            char character = text[i];
-            float uv_x = (character%16)/16.0f;
-            float uv_y = (character/16)/16.0f;
+		vertices.push_back(vertex_down_right);
+		vertices.push_back(vertex_up_right);
+		vertices.push_back(vertex_down_left);
 
-            glm::vec2 uv_up_left    = glm::vec2( uv_x           , uv_y );
-            glm::vec2 uv_up_right   = glm::vec2( uv_x+1.0f/16.0f, uv_y );
-            glm::vec2 uv_down_right = glm::vec2( uv_x+1.0f/16.0f, (uv_y + 1.0f/16.0f) );
-            glm::vec2 uv_down_left  = glm::vec2( uv_x           , (uv_y + 1.0f/16.0f) );
-            UVs.push_back(uv_up_left   );
-            UVs.push_back(uv_down_left );
-            UVs.push_back(uv_up_right  );
+		char character = text[i];
+		float uv_x = (character%16)/16.0f;
+		float uv_y = (character/16)/16.0f;
 
-            UVs.push_back(uv_down_right);
-            UVs.push_back(uv_up_right);
-            UVs.push_back(uv_down_left);
-        }
+		glm::vec2 uv_up_left    = glm::vec2( uv_x           , uv_y );
+		glm::vec2 uv_up_right   = glm::vec2( uv_x+1.0f/16.0f, uv_y );
+		glm::vec2 uv_down_right = glm::vec2( uv_x+1.0f/16.0f, (uv_y + 1.0f/16.0f) );
+		glm::vec2 uv_down_left  = glm::vec2( uv_x           , (uv_y + 1.0f/16.0f) );
+		UVs.push_back(uv_up_left   );
+		UVs.push_back(uv_down_left );
+		UVs.push_back(uv_up_right  );
 
-        glGenVertexArrays(1, &text_vertex_buffer_array_);
-        glBindVertexArray(text_vertex_buffer_array_);
+		UVs.push_back(uv_down_right);
+		UVs.push_back(uv_up_right);
+		UVs.push_back(uv_down_left);
+	}
 
-        glBindBuffer(GL_ARRAY_BUFFER, Text2DVertexBufferID);
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), &vertices[0], GL_STATIC_DRAW);
-        glBindBuffer(GL_ARRAY_BUFFER, Text2DUVBufferID);
-        glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs[0], GL_STATIC_DRAW);
-        num_vertices = vertices.size();
-        
-        // Bind shader
+	glGenVertexArrays(1, &text_vertex_buffer_array_);
+	glBindVertexArray(text_vertex_buffer_array_);
 
-        // Bind texture
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, Text2DTextureID);
-        // Set our "myTextureSampler" sampler to use Texture Unit 0
-        glUniform1i(Text2DUniformID, 0);
-    }
+	glBindBuffer(GL_ARRAY_BUFFER, Text2DVertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), &vertices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, Text2DUVBufferID);
+	glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs[0], GL_STATIC_DRAW);
+	num_vertices = vertices.size();
+	
+	// Bind shader
+
+	// Bind texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Text2DTextureID);
+	// Set our "myTextureSampler" sampler to use Texture Unit 0
+	glUniform1i(Text2DUniformID, 0);
 
 	glUseProgram(Text2DShaderID);
     glBindVertexArray(text_vertex_buffer_array_);
@@ -227,5 +222,7 @@ void printText2D(const char* text, const float x, const float y, const float siz
 	glDisableVertexAttribArray(1);
     glBindVertexArray(0);
 
-	glUseProgram(0);
+	glDeleteVertexArrays(1, &text_vertex_buffer_array_);
+
+	// glUseProgram(0);
 }
