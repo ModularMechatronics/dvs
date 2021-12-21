c57579483494bf6b983a6f73e6473dd4b47109cc
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-12-21 05:35:12 +0100

    Testing text rendering...

---
+++ cpp/applications/improvements/text_rendering_new.h
@@ -0,0 +1,13 @@
+#ifndef TEXT_RENDERING_NEW_H_
+#define TEXT_RENDERING_NEW_H_
+
+#include <glm/glm.hpp>
+
+#include <string>
+
+#include "opengl_low_level/opengl_low_level.h"
+
+bool initFreetype();
+void renderText(GLuint shader_id, std::string text, float x, float y, float scale, glm::vec3 color);
+
+#endif
