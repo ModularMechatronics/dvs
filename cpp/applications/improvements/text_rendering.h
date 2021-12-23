9426282b7babde018dd0cef8a46f7333883b879f
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2021-12-23 07:40:05 +0100

    Scaling of text works

--- cpp/applications/improvements/text_rendering.h
+++ cpp/applications/improvements/text_rendering.h
@@ -8,6 +8,6 @@
 #include "opengl_low_level/opengl_low_level.h"
 
 bool initFreetype();
-void renderText(GLuint shader_id, std::string text, float x, float y, float scale, glm::vec3 color);
+void renderText(GLuint shader_id, std::string text, float x, float y, float scale, const float width, const float height, glm::vec3 color);
 
 #endif
