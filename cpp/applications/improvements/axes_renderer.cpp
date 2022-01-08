6fecf2be5d66d4d38850b8f2fde3afdad4d6ef29
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-01-08 08:08:12 +0100

    Changes

--- cpp/applications/improvements/axes_renderer.cpp
+++ cpp/applications/improvements/axes_renderer.cpp
@@ -76,7 +76,6 @@ AxesRenderer::AxesRenderer(const AxesSettings& axes_settings) : axes_settings_(a
     plot_box_walls_ = new PlotBoxWalls(1.0f);
     plot_box_silhouette_ = new PlotBoxSilhouette(1.0f);
     plot_box_grid_ = new PlotBoxGrid(1.0f);
-    plot_box_grid_numbers_ = new PlotBoxGridNumbers(0.1f);
     half_cube_ = VboWrapper3D(half_cube_vertices_num_vertices, half_cube_vertices, half_cube_color);
 
     const std::string v_path = "../applications/improvements/shaders/basic.vertex";
@@ -117,9 +116,9 @@ void AxesRenderer::render()
     renderBoxGrid();
     renderBoxGridNumbers();
 
-    // plotBegin();
-    // half_cube_.render();
-    // plotEnd();
+    plotBegin();
+    half_cube_.render();
+    plotEnd();
 }
 
 void drawXAxisNumbers(const glm::mat4& view_model,
@@ -130,26 +129,23 @@ void drawXAxisNumbers(const glm::mat4& view_model,
                       const float width,
                       const float height,
                       const Vec3Dd& axes_center,
-                      const GridVectors& gv,
-                      GLuint shader_id)
+                      const GridVectors& gv)
 {
     const bool cond = (azimuth > (M_PI / 2.0)) || (azimuth < (-M_PI / 2.0));
     const double y = cond ? 1.0 : -1.0;
     const double z = elevation > 0.0 ? -1.0 : 1.0;
 
-    glm::vec3 color(0.0, 0.0, 0.0);
-
     for(size_t k = 0; k < gv.x.size(); k++)
     {
         const double x = gv.x(k);
-        glm::vec3 v3(x, y, z);
+        const glm::vec3 v3(x, y, z);
 
-        glm::vec3 v_projected = glm::project(v3,
+        const glm::vec3 v_projected = glm::project(v3,
                          view_model,
                          projection,
                          v_viewport);
         const std::string val = formatNumber(gv.x(k) + axes_center.x, 3);
-        renderText(shader_id, val, v_projected[0], v_projected[1], 0.0005f, width, height, color);
+        renderText(val, v_projected[0], v_projected[1], 0.0005f, width, height);
     }
 }
 
@@ -161,25 +157,22 @@ void drawYAxisNumbers(const glm::mat4& view_model,
                       const float width,
                       const float height,
                       const Vec3Dd& axes_center,
-                      const GridVectors& gv,
-                      GLuint shader_id)
+                      const GridVectors& gv)
 {
     const double x = azimuth < 0.0 ? 1.0 : -1.0;
     const double z = elevation > 0.0 ? -1.0 : 1.0;
 
-    glm::vec3 color(0.0, 0.0, 0.0);
-
     for(size_t k = 0; k < gv.y.size(); k++)
     {
         const double y = gv.y(k);
-        glm::vec3 v3(x, y, z);
+        const glm::vec3 v3(x, y, z);
 
-        glm::vec3 v_projected = glm::project(v3,
+        const glm::vec3 v_projected = glm::project(v3,
                          view_model,
                          projection,
                          v_viewport);
         const std::string val = formatNumber(gv.y(k) + axes_center.y, 3);
-        renderText(shader_id, val, v_projected[0], v_projected[1], 0.0005f, width, height, color);
+        renderText(val, v_projected[0], v_projected[1], 0.0005f, width, height);
     }
 }
 
@@ -191,26 +184,23 @@ void drawZAxisNumbers(const glm::mat4& view_model,
                       const float width,
                       const float height,
                       const Vec3Dd& axes_center,
-                      const GridVectors& gv,
-                      GLuint shader_id)
+                      const GridVectors& gv)
 {
     const bool cond = (azimuth > (M_PI / 2.0)) || (azimuth < (-M_PI / 2.0));
     const double x = azimuth > 0.0f ? 1.0 : -1.0;
     const double y = cond ? 1.0 : -1.0;
 
-    glm::vec3 color(0.0, 0.0, 0.0);
-
     for(size_t k = 0; k < gv.z.size(); k++)
     {
         const double z = gv.z(k);
-        glm::vec3 v3(x, y, z);
+        const glm::vec3 v3(x, y, z);
 
-        glm::vec3 v_projected = glm::project(v3,
+        const glm::vec3 v_projected = glm::project(v3,
                          view_model,
                          projection,
                          v_viewport);
         const std::string val = formatNumber(gv.z(k) + axes_center.z, 3);
-        renderText(shader_id, val, v_projected[0], v_projected[1], 0.0005f, width, height, color);
+        renderText(val, v_projected[0], v_projected[1], 0.0005f, width, height);
     }
 }
 
@@ -218,22 +208,18 @@ void AxesRenderer::renderBoxGridNumbers()
 {
     glUseProgram(text_shader_.programId());
 
+    glm::vec3 color(0.0, 0.0, 0.0);
+    glUniform3f(glGetUniformLocation(text_shader_.programId(), "textColor"), color.x, color.y, color.z);
+
     // AxesLimits
     const Vec3Dd axes_center = axes_limits_.getAxesCenter();
 
     // Scales
     const Vec3Dd scale = axes_limits_.getAxesScale();
-    const Vec3Dd s = axes_settings_.getAxesScale();
 
     model_mat[3][0] = 0.0;
     model_mat[3][1] = 0.0;
     model_mat[3][2] = 0.0;
-
-    const float sc = 0.002f;
-    const Vec2Df offset(0.5f, 0.5f);
-    const Vec2Df text_size = calculateStringSize("A This is text", offset.x, offset.y, sc, width_, height_);
-
-    renderText(text_shader_.programId(), "A This is text", offset.x, offset.y, sc, width_, height_, glm::vec3(0.5, 0.8f, 0.2f));
 
     const glm::vec4 v_viewport = glm::vec4(-1, -1, 2, 2);
 
@@ -255,17 +241,10 @@ void AxesRenderer::renderBoxGridNumbers()
     scale_mat[2][2] = 1.0 / scale.z;
     const glm::mat4 view_model_z = view_mat * model_mat * scale_mat;
 
-    // drawXAxisNumbers(view_model_x, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_, text_shader_.programId());
-    drawYAxisNumbers(view_model_y, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_, text_shader_.programId());
-    // drawZAxisNumbers(view_model_z, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_, text_shader_.programId());
+    drawXAxisNumbers(view_model_x, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_);
+    drawYAxisNumbers(view_model_y, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_);
+    drawZAxisNumbers(view_model_z, v_viewport, projection_mat, az, el, width_, height_, axes_center, gv_);
 
-    /*plot_box_grid_numbers_->render(gv_,
-                                   axes_settings_,
-                                   axes_limits_,
-                                   view_angles_,
-                                   coord_converter_,
-                                   width_,
-                                   height_);*/
     glUseProgram(0);
 }
 
