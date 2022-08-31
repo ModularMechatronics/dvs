9fe4e97bde4f5dd9876bc37047071a6dea1d98b1
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-08-31 09:42:53 +0200

    Added advanced tests cpp

---
+++ src/test_applications/advanced_tests_cpp/tests/plot2.h
@@ -0,0 +1,16 @@
+#ifndef TEST_APPLICATIONS_ADVANCED_TESTS_CPP_PLOT2_H_
+#define TEST_APPLICATIONS_ADVANCED_TESTS_CPP_PLOT2_H_
+
+using namespace dvs;
+
+namespace plot2
+{
+
+void testBasic()
+{
+    std::cout << "from testBasic!" << std::endl;
+}
+
+}
+
+#endif // TEST_APPLICATIONS_ADVANCED_TESTS_CPP_PLOT2_H_
