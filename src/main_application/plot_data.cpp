325e2c649df2cfeb767a570bb3d28584f1df29ea
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-09-07 10:29:05 +0200

    Cleanup

--- src/main_application/plot_data.cpp
+++ src/main_application/plot_data.cpp
@@ -27,8 +27,7 @@ void PlotDataHandler::clear()
 
 void PlotDataHandler::addData(std::unique_ptr<const ReceivedData> received_data, const TransmissionHeader& hdr)
 {
-    const TransmissionHeaderObject fcn_obj = hdr.getObjectAtIdx(0);
-    Function fcn = fcn_obj.as<Function>();
+    const Function fcn = hdr.getObjectAtIdx(0).as<Function>();
 
     if (pending_clear_)
     {
