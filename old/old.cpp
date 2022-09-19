34ad1018a2eb499753b6a54109dc7017765d9cbc
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-09-19 15:18:56 +0200

    Changes

--- old/old.cpp
+++ old/old.cpp
@@ -4,6 +4,75 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+static Vec3d findScale(const glm::mat4& pm)
+{
+    // Currently unknown exactly how 'q' affects the results...
+    const double q = 1.0;
+    // clang-format off
+    const Vector<glm::vec4> points{VectorInitializer{{glm::vec4{q, q, q, 1.0},
+                                     glm::vec4{-q, q, q, 1.0},
+                                     glm::vec4{q, -q, q, 1.0},
+                                     glm::vec4{-q, -q, q, 1.0},
+                                     glm::vec4{q, q, -q, 1.0},
+                                     glm::vec4{-q, q, -q, 1.0},
+                                     glm::vec4{q, -q, -q, 1.0},
+                                     glm::vec4{-q, -q, -q, 1.0}}}};
+    const glm::vec4 pr0 = pm * points(0);
+    // clang-format on
+    std::pair<glm::vec4, glm::vec4> pmiw = {points(0), pr0}, pmaw = {points(0), pr0}, pmih = {points(0), pr0},
+                                  pmah = {points(0), pr0};
+    for (const glm::vec4& p : points)
+    {
+        auto pqr = pm * p;
+        pqr /= pqr.w;
+        pqr = pqr * 0.5f + 0.5f;
+        pqr = pqr * 2.0f + 1.0f;
+
+        if (pqr.x < pmiw.second.x)
+        {
+            pmiw.first = p;
+            pmiw.second = pqr;
+        }
+
+        if (pqr.x > pmaw.second.x)
+        {
+            pmaw.first = p;
+            pmaw.second = pqr;
+        }
+
+        if (pqr.y < pmih.second.y)
+        {
+            pmih.first = p;
+            pmih.second = pqr;
+        }
+
+        if (pqr.y > pmah.second.y)
+        {
+            pmah.first = p;
+            pmah.second = pqr;
+        }
+    }
+
+    // const double r00 = R(0, 0), r01 = R(0, 1), r02 = R(0, 2);
+    // const double r10 = R(1, 0), r11 = R(1, 1), r12 = R(1, 2);
+    // const double r20 = R(2, 0), r21 = R(2, 1), r22 = R(2, 2);
+    const double q00 = pm[0][0], q01 = pm[0][1], q02 = pm[0][2], q03 = pm[0][3];
+    const double q10 = pm[1][0], q11 = pm[1][1], q12 = pm[1][2], q13 = pm[1][3];
+    const double q20 = pm[2][0], q21 = pm[2][1], q22 = pm[2][2], q23 = pm[2][3];
+    const double q30 = pm[3][0], q31 = pm[3][1], q32 = pm[3][2], q33 = pm[3][3];
+    const double w = 0.1, h = 0.1;
+    const double pmiw_x = pmiw.first.x, pmiw_y = pmiw.first.y, pmiw_z = pmiw.first.z;
+    const double pmih_x = pmih.first.x, pmih_y = pmih.first.y, pmih_z = pmih.first.z;
+
+    const double sx = (pmih_y*q03*q11 - pmiw_y*q01*q13 + h*pmih_y*q03*q31 - h*pmiw_y*q01*q33 + pmih_y*pmiw_z*q02*q11 - pmih_z*pmiw_y*q01*q12 + pmih_y*q11*q33*w - pmiw_y*q13*q31*w + h*pmih_y*pmiw_z*q02*q31 - h*pmih_z*pmiw_y*q01*q32 + h*pmih_y*q31*q33*w - h*pmiw_y*q31*q33*w + pmih_y*pmiw_z*q11*q32*w - pmih_z*pmiw_y*q12*q31*w + h*pmih_y*pmiw_z*q31*q32*w - h*pmih_z*pmiw_y*q31*q32*w)/(pmih_x*pmiw_y*q01*q10 - pmih_y*pmiw_x*q00*q11 + h*pmih_x*pmiw_y*q01*q30 - h*pmih_y*pmiw_x*q00*q31 + pmih_x*pmiw_y*q10*q31*w - pmih_y*pmiw_x*q11*q30*w + h*pmih_x*pmiw_y*q30*q31*w - h*pmih_y*pmiw_x*q30*q31*w);
+    const double sy = -(pmih_x*q03*q10 - pmiw_x*q00*q13 + h*pmih_x*q03*q30 - h*pmiw_x*q00*q33 + pmih_x*pmiw_z*q02*q10 - pmih_z*pmiw_x*q00*q12 + pmih_x*q10*q33*w - pmiw_x*q13*q30*w + h*pmih_x*pmiw_z*q02*q30 - h*pmih_z*pmiw_x*q00*q32 + h*pmih_x*q30*q33*w - h*pmiw_x*q30*q33*w + pmih_x*pmiw_z*q10*q32*w - pmih_z*pmiw_x*q12*q30*w + h*pmih_x*pmiw_z*q30*q32*w - h*pmih_z*pmiw_x*q30*q32*w)/(pmih_x*pmiw_y*q01*q10 - pmih_y*pmiw_x*q00*q11 + h*pmih_x*pmiw_y*q01*q30 - h*pmih_y*pmiw_x*q00*q31 + pmih_x*pmiw_y*q10*q31*w - pmih_y*pmiw_x*q11*q30*w + h*pmih_x*pmiw_y*q30*q31*w - h*pmih_y*pmiw_x*q30*q31*w);
+    const double sz = 1.0;
+
+    // std::cout << Vec3d(sx, sy, sz) << std::endl;
+
+    return Vec3d(sx, sy, sz);
+}
+
 inline void sleepMS(const int ms)
 {
     #include <stdlib.h>
