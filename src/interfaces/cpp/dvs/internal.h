5483a04b20d09540cc55edc3b38998d229c97185
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-11-10 22:49:47 +0100

    Changes

--- src/interfaces/cpp/dvs/internal.h
+++ src/interfaces/cpp/dvs/internal.h
@@ -220,68 +220,6 @@ void sendHeaderAndVectorCollection(const SendFunctionType& send_function,
     send_function(fillable_array.view());
 }
 
-template <typename T>
-void fillBufferWithRefCollection(FillableUInt8Array& fillable_array,
-                                 const std::vector<std::reference_wrapper<Vector<T>>>& data_to_be_sent)
-{
-    for (size_t k = 0; k < data_to_be_sent.size(); k++)
-    {
-        const Vector<T>& data_ref = data_to_be_sent[k];
-        fillable_array.fillWithDataFromPointer(data_ref.data(), data_ref.numElements());
-    }
-}
-
-template <typename T, typename... Us>
-void fillBufferWithRefCollection(FillableUInt8Array& fillable_array,
-                                 const std::vector<std::reference_wrapper<Vector<T>>>& data_to_be_sent,
-                                 const Us&... other_elements)
-{
-    for (size_t k = 0; k < data_to_be_sent.size(); k++)
-    {
-        const Vector<T>& data_ref = data_to_be_sent[k];
-        fillable_array.fillWithDataFromPointer(data_ref.data(), data_ref.numElements());
-    }
-    fillBufferWithRefCollection(fillable_array, other_elements...);
-}
-
-template <typename T, typename... Us>
-void sendHeaderAndRefVectorCollection(const SendFunctionType& send_function,
-                                      const CommunicationHeader& hdr,
-                                      const Vector<uint16_t>& vector_lengths,
-                                      const size_t num_bytes_to_send,
-                                      const std::vector<std::reference_wrapper<Vector<T>>>& first_element,
-                                      const Us&... other_elements)
-{
-    const uint64_t num_bytes_hdr = hdr.numBytes();
-    const uint64_t num_bytes_from_object = num_bytes_to_send + num_bytes_hdr;
-
-    // + 1 bytes for endianness byte
-    // + 2 * sizeof(uint64_t) for magic number and number of bytes in transfer
-    const uint64_t num_bytes = num_bytes_from_object + 1 + 2 * sizeof(uint64_t) + vector_lengths.numBytes();
-
-    FillableUInt8Array fillable_array{num_bytes};
-
-    fillable_array.fillWithStaticType(isBigEndian());
-
-    fillable_array.fillWithStaticType(kMagicNumber);
-
-    fillable_array.fillWithStaticType(num_bytes);
-
-    hdr.fillBufferWithData(fillable_array);
-
-    fillable_array.fillWithDataFromPointer(vector_lengths.data(), vector_lengths.numElements());
-
-    for (size_t k = 0; k < first_element.size(); k++)
-    {
-        const Vector<T>& fe_ref = first_element[k];
-        fillable_array.fillWithDataFromPointer(fe_ref.data(), fe_ref.numElements());
-    }
-
-    fillBufferWithRefCollection(fillable_array, other_elements...);
-
-    send_function(fillable_array.view());
-}
-
 inline void sendHeaderOnly(const SendFunctionType& send_function, const CommunicationHeader& hdr)
 {
     const uint64_t num_bytes_hdr = hdr.numBytes();
