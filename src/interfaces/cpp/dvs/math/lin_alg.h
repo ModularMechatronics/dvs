4243c514c86d1f742b3adc6d4eb5af97fb47d97f
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-11-11 15:36:11 +0100

    Changes

--- src/interfaces/cpp/dvs/math/lin_alg.h
+++ src/interfaces/cpp/dvs/math/lin_alg.h
@@ -2,6 +2,7 @@
 #define DVS_MATH_LIN_ALG_H_
 
 #include "dvs/math/lin_alg/matrix_dynamic/matrix_dynamic.h"
+#include "dvs/math/lin_alg/matrix_fixed/matrix_fixed.h"
 #include "dvs/math/lin_alg/matrix_vector_dynamic.h"
 #include "dvs/math/lin_alg/vector_dynamic/vector_dynamic.h"
 #include "dvs/math/lin_alg/vector_low_dim/vec2.h"
