1044c7222e771f491ba42e1ce3019c9e68bb8ff8
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2022-12-05 13:44:50 +0100

    Changes

--- src/main_application/project_state/project_settings.h
+++ src/main_application/project_state/project_settings.h
@@ -9,6 +9,25 @@
 #include "dvs/logging.h"
 #include "opengl_low_level/data_structures.h"
 
+constexpr RGBTriplet<float> kElementBackgroundColorDefault{0.543f, 0.751f, 0.684f};
+constexpr RGBTriplet<float> kPlotBoxColorDefault{1.0f, 1.0f, 1.0f};
+constexpr RGBTriplet<float> kGridColorDefault{0.7f, 0.7f, 0.7f};
+constexpr RGBTriplet<float> kAxesNumbersColorDefault{0.0f, 0.0f, 0.0f};
+constexpr RGBTriplet<float> kAxesLettersColorDefault{0.0f, 0.0f, 0.0f};
+constexpr bool kGridOnDefault{true};
+constexpr bool kPlotBoxOnDefault{true};
+constexpr bool kAxesNumbersOnDefault{true};
+constexpr bool kAxesLettersOnDefault{true};
+constexpr bool kClippingOnDefault{true};
+constexpr float kPaneRadiusDefault{10.0f};
+constexpr int kZOrderDefault{-1};
+
+constexpr RGBTriplet<float> kTabBackgroundColorDefault{0.643f, 0.851f, 0.784f};
+constexpr RGBTriplet<float> kButtonNormalColorDefault{0.682f, 0.890f, 0.820f};
+constexpr RGBTriplet<float> kButtonClickedColorDefault{0.682f, 0.890f, 0.820f};
+constexpr RGBTriplet<float> kButtonSelectedColorDefault{0.643f, 0.851f, 0.780f};
+constexpr RGBTriplet<float> kButtonTextColorDefault{0.0f, 0.0f, 0.0f};
+
 inline void throwIfMissing(const nlohmann::json& j, const std::string& field_name, const std::string& exception_string)
 {
     if (j.count(field_name) == 0)
@@ -29,6 +48,15 @@ inline nlohmann::json colorToJsonObj(const RGBTripletf& c)
     j["g"] = c.green;
     j["b"] = c.blue;
     return j;
+}
+
+template <typename T>
+void assignIfNotDefault(nlohmann::json& j, const std::string& key, const T& val, const T& default_value)
+{
+    if (val != default_value)
+    {
+        j[key] = val;
+    }
 }
 
 struct ElementSettings
@@ -63,23 +91,23 @@ struct ElementSettings
         width = 100.0f;
         height = 100.0f;
 
-        z_order = -1;
-
-        pane_radius = 10.0f;
+        pane_radius = kPaneRadiusDefault;
 
         name = "<NO-NAME>";
 
-        background_color = RGBTripletf(164.0f / 255.0f, 217.0f / 255.0f, 200.0f / 255.0f);
-        plot_box_color = RGBTripletf(1.0f, 1.0f, 1.0f);
-        grid_color = RGBTripletf(0.7f, 0.7f, 0.7f);
-        axes_numbers_color = RGBTripletf(0.7f, 0.7f, 0.7f);
-        axes_letters_color = RGBTripletf(0.7f, 0.7f, 0.7f);
+        background_color = kElementBackgroundColorDefault;
+        plot_box_color = kPlotBoxColorDefault;
+        grid_color = kGridColorDefault;
+        axes_numbers_color = kAxesNumbersColorDefault;
+        axes_letters_color = kAxesLettersColorDefault;
 
-        grid_on = true;
-        plot_box_on = true;
-        axes_numbers_on = true;
-        axes_letters_on = true;
-        clipping_on = true;
+        grid_on = kGridOnDefault;
+        plot_box_on = kPlotBoxOnDefault;
+        axes_numbers_on = kAxesNumbersOnDefault;
+        axes_letters_on = kAxesLettersOnDefault;
+        clipping_on = kClippingOnDefault;
+
+        z_order = kZOrderDefault;
     }
 
     explicit ElementSettings(const nlohmann::json& j)
@@ -97,24 +125,24 @@ struct ElementSettings
         x = std::max(std::min(x, 0.9f), 0.0f);
         y = std::max(std::min(y, 0.9f), 0.0f);
 
-        background_color = (j.count("background_color") > 0)
-                               ? jsonObjToColor(j["background_color"])
-                               : RGBTripletf(164.0f / 255.0f, 217.0f / 255.0f, 200.0f / 255.0f);
-        plot_box_color =
-            (j.count("plot_box_color") > 0) ? jsonObjToColor(j["plot_box_color"]) : RGBTripletf(1.0f, 1.0f, 1.0f);
-        grid_color = (j.count("grid_color") > 0) ? jsonObjToColor(j["grid_color"]) : RGBTripletf(0.7f, 0.7f, 0.7f);
-        axes_numbers_color = (j.count("axes_numbers_color") > 0) ? jsonObjToColor(j["axes_numbers_color"])
-                                                                 : RGBTripletf(0.0f, 0.0f, 0.0f);
-        axes_letters_color = (j.count("axes_letters_color") > 0) ? jsonObjToColor(j["axes_letters_color"])
-                                                                 : RGBTripletf(0.0f, 0.0f, 0.0f);
-        grid_on = (j.count("grid_on") > 0) ? static_cast<bool>(j["grid_on"]) : true;
-        plot_box_on = (j.count("plot_box_on") > 0) ? static_cast<bool>(j["plot_box_on"]) : true;
-        axes_numbers_on = (j.count("axes_numbers_on") > 0) ? static_cast<bool>(j["axes_numbers_on"]) : true;
-        axes_letters_on = (j.count("axes_letters_on") > 0) ? static_cast<bool>(j["axes_letters_on"]) : true;
-        clipping_on = (j.count("clipping_on") > 0) ? static_cast<bool>(j["clipping_on"]) : true;
-        pane_radius = (j.count("pane_radius") > 0) ? static_cast<float>(j["pane_radius"]) : 10.0f;
+        background_color =
+            (j.count("background_color") > 0) ? jsonObjToColor(j["background_color"]) : kElementBackgroundColorDefault;
+        plot_box_color = (j.count("plot_box_color") > 0) ? jsonObjToColor(j["plot_box_color"]) : kPlotBoxColorDefault;
+        grid_color = (j.count("grid_color") > 0) ? jsonObjToColor(j["grid_color"]) : kGridColorDefault;
+        axes_numbers_color =
+            (j.count("axes_numbers_color") > 0) ? jsonObjToColor(j["axes_numbers_color"]) : kAxesNumbersColorDefault;
+        axes_letters_color =
+            (j.count("axes_letters_color") > 0) ? jsonObjToColor(j["axes_letters_color"]) : kAxesLettersColorDefault;
+        grid_on = (j.count("grid_on") > 0) ? static_cast<bool>(j["grid_on"]) : kGridOnDefault;
+        plot_box_on = (j.count("plot_box_on") > 0) ? static_cast<bool>(j["plot_box_on"]) : kPlotBoxOnDefault;
+        axes_numbers_on =
+            (j.count("axes_numbers_on") > 0) ? static_cast<bool>(j["axes_numbers_on"]) : kAxesNumbersOnDefault;
+        axes_letters_on =
+            (j.count("axes_letters_on") > 0) ? static_cast<bool>(j["axes_letters_on"]) : kAxesLettersOnDefault;
+        clipping_on = (j.count("clipping_on") > 0) ? static_cast<bool>(j["clipping_on"]) : kClippingOnDefault;
+        pane_radius = (j.count("pane_radius") > 0) ? static_cast<float>(j["pane_radius"]) : kPaneRadiusDefault;
 
-        z_order = (j.count("z_order") > 0) ? static_cast<int>(j["z_order"]) : -1;
+        z_order = (j.count("z_order") > 0) ? static_cast<int>(j["z_order"]) : kZOrderDefault;
     }
 
     nlohmann::json toJson() const
@@ -128,19 +156,38 @@ struct ElementSettings
         j["width"] = width;
         j["height"] = height;
 
-        j["z_order"] = z_order;
+        if (background_color != kElementBackgroundColorDefault)
+        {
+            j["background_color"] = colorToJsonObj(background_color);
+        }
 
-        j["background_color"] = colorToJsonObj(background_color);
-        j["plot_box_color"] = colorToJsonObj(plot_box_color);
-        j["grid_color"] = colorToJsonObj(grid_color);
-        j["axes_numbers_color"] = colorToJsonObj(axes_numbers_color);
-        j["axes_letters_color"] = colorToJsonObj(axes_letters_color);
-        j["grid_on"] = grid_on;
-        j["plot_box_on"] = plot_box_on;
-        j["axes_numbers_on"] = axes_numbers_on;
-        j["axes_letters_on"] = axes_letters_on;
-        j["clipping_on"] = clipping_on;
-        j["pane_radius"] = pane_radius;
+        if (background_color != kPlotBoxColorDefault)
+        {
+            j["plot_box_color"] = colorToJsonObj(plot_box_color);
+        }
+
+        if (background_color != kGridColorDefault)
+        {
+            j["grid_color"] = colorToJsonObj(grid_color);
+        }
+
+        if (background_color != kAxesNumbersColorDefault)
+        {
+            j["axes_numbers_color"] = colorToJsonObj(axes_numbers_color);
+        }
+
+        if (background_color != kAxesLettersColorDefault)
+        {
+            j["axes_letters_color"] = colorToJsonObj(axes_letters_color);
+        }
+
+        assignIfNotDefault(j, "grid_on", grid_on, kGridOnDefault);
+        assignIfNotDefault(j, "plot_box_on", plot_box_on, kPlotBoxOnDefault);
+        assignIfNotDefault(j, "axes_numbers_on", axes_numbers_on, kAxesNumbersOnDefault);
+        assignIfNotDefault(j, "axes_letters_on", axes_letters_on, kAxesLettersOnDefault);
+        assignIfNotDefault(j, "clipping_on", clipping_on, kClippingOnDefault);
+        assignIfNotDefault(j, "pane_radius", pane_radius, kPaneRadiusDefault);
+        assignIfNotDefault(j, "z_order", z_order, kZOrderDefault);
 
         return j;
     }
@@ -169,11 +216,11 @@ struct TabSettings
 
     TabSettings()
     {
-        background_color = RGBTripletf(35.0f / 255.0f, 42.0f / 255.0f, 48.0f / 255.0f);
-        button_normal_color = RGBTripletf(174.0f / 255.0f, 227.0f / 255.0f, 209.0f / 255.0f);
-        button_clicked_color = RGBTripletf(174.0f / 255.0f, 227.0f / 255.0f, 209.0f / 255.0f);
-        button_selected_color = RGBTripletf(164.0f / 255.0f, 217.0f / 255.0f, 199.0f / 255.0f);
-        button_text_color = RGBTripletf(0.0f, 0.0f, 0.0f);
+        background_color = kTabBackgroundColorDefault;
+        button_normal_color = kButtonNormalColorDefault;
+        button_clicked_color = kButtonClickedColorDefault;
+        button_selected_color = kButtonSelectedColorDefault;
+        button_text_color = kButtonTextColorDefault;
     }
 
     TabSettings(const nlohmann::json& j)
@@ -185,20 +232,16 @@ struct TabSettings
             elements.emplace_back(j["elements"][k]);
         }
 
-        background_color = j.count("background_color") > 0
-                               ? jsonObjToColor(j["background_color"])
-                               : RGBTripletf(35.0f / 255.0f, 42.0f / 255.0f, 48.0f / 255.0f);
-        button_normal_color = j.count("button_normal_color") > 0
-                                  ? jsonObjToColor(j["button_normal_color"])
-                                  : RGBTripletf(174.0f / 255.0f, 227.0f / 255.0f, 209.0f / 255.0f);
-        button_clicked_color = j.count("button_clicked_color") > 0
-                                   ? jsonObjToColor(j["button_clicked_color"])
-                                   : RGBTripletf(174.0f / 255.0f, 227.0f / 255.0f, 209.0f / 255.0f);
-        button_selected_color = j.count("button_selected_color") > 0
-                                    ? jsonObjToColor(j["button_selected_color"])
-                                    : RGBTripletf(164.0f / 255.0f, 217.0f / 255.0f, 199.0f / 255.0f);
+        background_color =
+            j.count("background_color") > 0 ? jsonObjToColor(j["background_color"]) : kTabBackgroundColorDefault;
+        button_normal_color =
+            j.count("button_normal_color") > 0 ? jsonObjToColor(j["button_normal_color"]) : kButtonNormalColorDefault;
+        button_clicked_color = j.count("button_clicked_color") > 0 ? jsonObjToColor(j["button_clicked_color"])
+                                                                   : kButtonClickedColorDefault;
+        button_selected_color = j.count("button_selected_color") > 0 ? jsonObjToColor(j["button_selected_color"])
+                                                                     : kButtonSelectedColorDefault;
         button_text_color =
-            j.count("button_text_color") > 0 ? jsonObjToColor(j["button_text_color"]) : RGBTripletf(0.0f, 0.0f, 0.0f);
+            j.count("button_text_color") > 0 ? jsonObjToColor(j["button_text_color"]) : kButtonTextColorDefault;
     }
 
     nlohmann::json toJson() const
@@ -214,11 +257,30 @@ struct TabSettings
         j["name"] = name;
         j["elements"] = json_elements;
 
-        j["background_color"] = colorToJsonObj(background_color);
-        j["button_normal_color"] = colorToJsonObj(button_normal_color);
-        j["button_clicked_color"] = colorToJsonObj(button_clicked_color);
-        j["button_selected_color"] = colorToJsonObj(button_selected_color);
-        j["button_text_color"] = colorToJsonObj(button_text_color);
+        if (background_color != kTabBackgroundColorDefault)
+        {
+            j["background_color"] = colorToJsonObj(background_color);
+        }
+
+        if (background_color != kButtonNormalColorDefault)
+        {
+            j["button_normal_color"] = colorToJsonObj(button_normal_color);
+        }
+
+        if (background_color != kButtonClickedColorDefault)
+        {
+            j["button_clicked_color"] = colorToJsonObj(button_clicked_color);
+        }
+
+        if (background_color != kButtonSelectedColorDefault)
+        {
+            j["button_selected_color"] = colorToJsonObj(button_selected_color);
+        }
+
+        if (background_color != kButtonTextColorDefault)
+        {
+            j["button_text_color"] = colorToJsonObj(button_text_color);
+        }
 
         return j;
     }
