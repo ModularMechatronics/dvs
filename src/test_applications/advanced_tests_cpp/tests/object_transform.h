36b15bc1c23c51a77cadd7f73617d1826c0d77d0
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2023-01-24 13:57:06 +0100

    Changes

--- src/test_applications/advanced_tests_cpp/tests/object_transform.h
+++ src/test_applications/advanced_tests_cpp/tests/object_transform.h
@@ -10,7 +10,48 @@ using namespace dvs;
 namespace object_transform
 {
 
-void testBasic()
+struct PointsAndIndices
+{
+    Vector<Point3f> points;
+    Vector<IndexTriplet> indices;
+};
+
+PointsAndIndices generateCube()
+{
+    PointsAndIndices pts_indices{};
+
+    // clang-format off
+    pts_indices.points = Vector<Point3f>{VectorInitializer<Point3f>{
+        {1.0, 1.0, 1.0},
+        {1.0, -1.0, 1.0},
+        {-1.0, -1.0, 1.0},
+        {-1.0, 1.0, 1.0},
+        {1.0, 1.0, -1.0},
+        {1.0, -1.0, -1.0},
+        {-1.0, -1.0, -1.0},
+        {-1.0, 1.0, -1.0},
+    }};
+
+    pts_indices.indices = Vector<IndexTriplet>{VectorInitializer<IndexTriplet>{
+        {0, 1, 2}, // Upper xy
+        {0, 2, 3},
+        {4, 6, 5}, // Lower xy
+        {4, 7, 6},
+        {0, 1, 4}, // Upper yz
+        {1, 5, 4},
+        {2, 6, 3}, // Lower yz
+        {6, 7, 3},
+        {0, 7, 3}, // Upper xz
+        {0, 4, 7},
+        {6, 2, 1}, // Lower xz
+        {6, 1, 5},
+    }};
+    // clang-format on
+
+    return pts_indices;
+}
+
+void testBasic2D()
 {
     const size_t num_elements = 5;
 
@@ -91,6 +132,78 @@ void testBasic()
         setTransform(properties::ID0, diagMatrix<double>(scale), r_mat, translation);
         usleep(50 * 1000);
     }
+}
+
+constexpr int delay_ms = 20;
+
+void changeScale(
+    Vec3d& scale, const Matrix<double>& r_mat, const Vec3d& translation, const Vec3d& inc, const size_t n_times)
+{
+    for (size_t k = 0; k < n_times; k++)
+    {
+        scale = scale + inc;
+
+        setTransform(properties::ID0, diagMatrix<double>(scale), r_mat, translation);
+        usleep(delay_ms * 1000);
+    }
+}
+
+void changeRotation(
+    const Vec3d& scale, Matrix<double>& r_mat, const Vec3d& translation, const Vec3d& inc, const size_t n_times)
+{
+    const Matrix<double> r_inc = rotationMatrixZ(inc.z) * rotationMatrixY(inc.y) * rotationMatrixX(inc.x);
+
+    for (size_t k = 0; k < n_times; k++)
+    {
+        r_mat = r_mat * r_inc;
+
+        setTransform(properties::ID0, diagMatrix<double>(scale), r_mat, translation);
+        usleep(delay_ms * 1000);
+    }
+}
+
+void changeTranslation(
+    const Vec3d& scale, const Matrix<double>& r_mat, Vec3d& translation, const Vec3d& inc, const size_t n_times)
+{
+    const Matrix<double> r_inc = rotationMatrixZ(inc.z) * rotationMatrixY(inc.y) * rotationMatrixX(inc.x);
+
+    for (size_t k = 0; k < n_times; k++)
+    {
+        translation = translation + inc;
+
+        setTransform(properties::ID0, diagMatrix<double>(scale), r_mat, translation);
+        usleep(delay_ms * 1000);
+    }
+}
+
+void testBasic3D()
+{
+    const size_t num_elements = 5;
+
+    const auto cube_pts_indices = generateCube();
+
+    constexpr double axis_limit = 10.0;
+
+    setCurrentElement("p_view_0");
+    clearView();
+    axis({-axis_limit, -axis_limit, -axis_limit}, {axis_limit, axis_limit, axis_limit});
+    drawMesh(cube_pts_indices.points, cube_pts_indices.indices, properties::FaceColor{242, 152, 124}, properties::ID0);
+
+    Vec3d translation{0.0, 0.0, 0.0};
+    Vec3d scale{1.0, 1.0, 1.0};
+    Matrix<double> r_mat = rotationMatrixZ(0.0);
+
+    changeScale(scale, r_mat, translation, Vec3d{0.1, 0.0, 0.0}, 50);
+    changeScale(scale, r_mat, translation, Vec3d{0.0, 0.1, 0.0}, 50);
+    changeScale(scale, r_mat, translation, Vec3d{0.0, 0.0, 0.1}, 50);
+
+    changeRotation(scale, r_mat, translation, Vec3d{0.01, 0.0, 0.0}, 50);
+    changeRotation(scale, r_mat, translation, Vec3d{0.0, 0.01, 0.0}, 50);
+    changeRotation(scale, r_mat, translation, Vec3d{0.0, 0.0, 0.01}, 50);
+
+    changeTranslation(scale, r_mat, translation, Vec3d{0.05, 0.0, 0.0}, 50);
+    changeTranslation(scale, r_mat, translation, Vec3d{0.0, 0.05, 0.0}, 50);
+    changeTranslation(scale, r_mat, translation, Vec3d{0.0, 0.0, 0.05}, 50);
 }
 
 }  // namespace object_transform
