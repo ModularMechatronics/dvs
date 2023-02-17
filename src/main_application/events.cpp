2343d79755bef1b0c6d1cf01c0ec769e8c66c5a0
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2023-02-17 14:00:57 +0100

    Changes

--- src/main_application/events.cpp
+++ src/main_application/events.cpp
@@ -1,6 +1,5 @@
 #include "events.h"
 
-wxDEFINE_EVENT(MY_EVENT, wxCommandEvent);
 wxDEFINE_EVENT(EDIT_EVENT, wxCommandEvent);
 wxDEFINE_EVENT(NO_ELEMENT_SELECTED, wxCommandEvent);
 wxDEFINE_EVENT(CHILD_WINDOW_IN_FOCUS_EVENT, wxCommandEvent);
--- src/main_application/io_devices/mouse_state.cpp
+++ src/main_application/io_devices/mouse_state.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-MouseButtonState::MouseButtonState()
+MouseState::MouseState()
 {
     is_pressed_ = false;
 
@@ -18,48 +18,12 @@ MouseButtonState::MouseButtonState()
     delta_pos_.y = 0;
 }
 
-/*Vec2i MouseButtonState::getCurrPos() const
-{
-    return curr_pos_;
-}
-
-Vec2i MouseButtonState::getPrevPos() const
-{
-    return prev_pos_;
-}*/
-
-Vec2i MouseButtonState::getDeltaPos() const
+Vec2i MouseState::getDeltaPos() const
 {
     return delta_pos_;
 }
 
-/*void MouseButtonState::setIsPressed(const int x, const int y)
-{
-    is_pressed_ = true;
-    curr_pos_.x = x;
-    curr_pos_.y = y;
-
-    prev_pos_.x = x;
-    prev_pos_.y = y;
-
-    delta_pos_.x = 0;
-    delta_pos_.y = 0;
-}
-
-void MouseButtonState::setIsReleased()
-{
-    is_pressed_ = false;
-    curr_pos_.x = -1;
-    curr_pos_.y = -1;
-
-    prev_pos_.x = -1;
-    prev_pos_.y = -1;
-
-    delta_pos_.x = 0;
-    delta_pos_.y = 0;
-}*/
-
-void MouseButtonState::setCurrentPos(const int current_x, const int current_y)
+void MouseState::setCurrentPos(const int current_x, const int current_y)
 {
     curr_pos_.x = current_x;
     curr_pos_.y = current_y;
@@ -71,10 +35,8 @@ void MouseButtonState::setCurrentPos(const int current_x, const int current_y)
     delta_pos_.y = 0;
 }
 
-void MouseButtonState::updateOnMotion(const int x, const int y)
+void MouseState::updateOnMotion(const int x, const int y)
 {
-    // if (is_pressed_)
-    // {
     prev_pos_.x = curr_pos_.x;
     prev_pos_.y = curr_pos_.y;
 
@@ -82,15 +44,9 @@ void MouseButtonState::updateOnMotion(const int x, const int y)
     curr_pos_.y = y;
 
     delta_pos_ = curr_pos_ - prev_pos_;
-    // }
 }
 
-/*bool MouseButtonState::isPressed() const
-{
-    return is_pressed_;
-}*/
-
-void MouseButtonState::print() const
+void MouseState::print() const
 {
     const std::string p = is_pressed_ ? "pressed" : "not pressed";
     std::cout << "Button is " << p << " with xy coords [ " << curr_pos_.x << ", " << curr_pos_.y << " ]" << std::endl;
