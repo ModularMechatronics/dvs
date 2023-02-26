129c1060bebab733f2cdcce187ee8c470a47bb66
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2023-02-26 21:22:57 +0100

    Changes

---
+++ src/main_application/input_data.cpp
@@ -0,0 +1,49 @@
+#include "input_data.h"
+
+InputData::InputData(ReceivedData& received_data)
+    : received_data_{std::move(received_data)},
+      function_{received_data_.getFunction()},
+      converted_data_{nullptr},
+      plot_object_attributes_{},
+      properties_data_{}
+{
+}
+
+InputData::InputData(ReceivedData& received_data,
+                     const PlotObjectAttributes& plot_object_attributes,
+                     const PropertiesData& properties_data)
+    : received_data_{std::move(received_data)},
+      function_{received_data_.getFunction()},
+      converted_data_{nullptr},
+      plot_object_attributes_{plot_object_attributes},
+      properties_data_{properties_data}
+{
+}
+
+InputData::InputData(ReceivedData& received_data,
+                     std::unique_ptr<const ConvertedDataBase>& converted_data,
+                     const PlotObjectAttributes& plot_object_attributes,
+                     const PropertiesData& properties_data)
+    : received_data_{std::move(received_data)},
+      function_{received_data_.getFunction()},
+      converted_data_{std::move(converted_data)},
+      plot_object_attributes_{plot_object_attributes},
+      properties_data_{properties_data}
+{
+}
+
+internal::Function InputData::getFunction() const
+{
+    return function_;
+}
+
+std::tuple<ReceivedData, PlotObjectAttributes, PropertiesData, std::unique_ptr<const ConvertedDataBase>>
+InputData::moveAllData()
+{
+    return {std::move(received_data_), plot_object_attributes_, properties_data_, std::move(converted_data_)};
+}
+
+std::tuple<ReceivedData, PlotObjectAttributes, PropertiesData> InputData::moveAllDataButConvertedData()
+{
+    return {std::move(received_data_), plot_object_attributes_, properties_data_};
+}
--- src/main_application/input_data.h
+++ src/main_application/input_data.h
@@ -12,53 +12,21 @@ class InputData
 public:
     InputData() = delete;
 
-    InputData(ReceivedData& received_data)
-        : received_data_{std::move(received_data)},
-          function_{received_data_.getFunction()},
-          converted_data_{nullptr},
-          plot_object_attributes_{},
-          properties_data_{}
-    {
-    }
-
+    InputData(ReceivedData& received_data);
     InputData(ReceivedData& received_data,
               const PlotObjectAttributes& plot_object_attributes,
-              const PropertiesData& properties_data)
-        : received_data_{std::move(received_data)},
-          function_{received_data_.getFunction()},
-          converted_data_{nullptr},
-          plot_object_attributes_{plot_object_attributes},
-          properties_data_{properties_data}
-    {
-    }
-
+              const PropertiesData& properties_data);
     InputData(ReceivedData& received_data,
               std::unique_ptr<const ConvertedDataBase>& converted_data,
               const PlotObjectAttributes& plot_object_attributes,
-              const PropertiesData& properties_data)
-        : received_data_{std::move(received_data)},
-          function_{received_data_.getFunction()},
-          converted_data_{std::move(converted_data)},
-          plot_object_attributes_{plot_object_attributes},
-          properties_data_{properties_data}
-    {
-    }
+              const PropertiesData& properties_data);
 
-    internal::Function getFunction() const
-    {
-        return function_;
-    }
+    internal::Function getFunction() const;
 
     std::tuple<ReceivedData, PlotObjectAttributes, PropertiesData, std::unique_ptr<const ConvertedDataBase>>
-    moveAllData()
-    {
-        return {std::move(received_data_), plot_object_attributes_, properties_data_, std::move(converted_data_)};
-    }
+    moveAllData();
 
-    std::tuple<ReceivedData, PlotObjectAttributes, PropertiesData> moveAllDataButConvertedData()
-    {
-        return {std::move(received_data_), plot_object_attributes_, properties_data_};
-    }
+    std::tuple<ReceivedData, PlotObjectAttributes, PropertiesData> moveAllDataButConvertedData();
 
 private:
     ReceivedData received_data_;
